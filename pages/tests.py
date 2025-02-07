d47bea5aa7fd58e3d743a1fedd65b23625da8aaa
Onifade Monsuru <onifade617@gmail.com>
2025-02-07 10:42:24 +0100

    second commit

---
+++ pages/tests.py
@@ -0,0 +1,29 @@
+from django.test import SimpleTestCase
+from django.urls import reverse
+
+from .views import HomePageView
+
+class HomepageTests(SimpleTestCase):
+    def setUp(self):
+        url = reverse('home')
+        self.response = self.client.get(url)
+
+    def test_homepage_status_code(self):
+        
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_homepage_url_name(self):
+       
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_homepage_template(self): # new
+        
+        self.assertTemplateUsed(self.response, 'home.html')
+
+    def test_homepage_contains_correct_html(self): # new
+        
+        self.assertContains(self.response, 'Homepage')
+
+    def test_homepage_does_not_contain_incorrect_html(self): # new
+        
+        self.assertNotContains(self.response, 'Hi there! I should not be on the page.')
