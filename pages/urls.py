b8450a9282a57f04526395cc00cb2957c1206e84
Onifade Monsuru <onifade617@gmail.com>
2025-02-10 12:03:45 +0100

    another commit

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from django.contrib import admin
+from django.urls import path, include
 
 from .views import HomePageView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
