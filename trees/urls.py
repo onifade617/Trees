092b205522f41399e63537174964d21f3b5f8c99
Onifade Monsuru <onifade617@gmail.com>
2025-02-24 11:48:44 +0100

    search bar

--- trees/urls.py
+++ trees/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import TreeListView, TreeDetailView
+from .views import TreeListView, TreeDetailView, SearchResultsListView
 
 urlpatterns = [
     path('', TreeListView.as_view(), name='tree_list'),
     path('<uuid:pk>', TreeDetailView.as_view(), name='tree_detail'),
+    path('search/', SearchResultsListView.as_view(), name='search_results'),
 ]
--- trees/views.py
+++ trees/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.contrib.auth.mixins import (LoginRequiredMixin, PermissionRequiredMixin)
 from django.views.generic import ListView, DetailView
 
@@ -17,3 +18,15 @@ class TreeDetailView(LoginRequiredMixin, PermissionRequiredMixin, DetailView): #
     template_name = 'trees/tree_detail.html'
     login_url = 'account_login'
     permission_required = 'trees.special_status'
+
+class SearchResultsListView(ListView): # new
+    model = Tree
+    context_object_name = 'tree_list'
+    template_name = 'trees/search_results.html'
+    
+
+    def get_queryset(self): # new
+        query = self.request.GET.get('q')
+        return Tree.objects.filter(
+            Q(title__icontains=query) | Q(farmer__icontains=query)
+        )
