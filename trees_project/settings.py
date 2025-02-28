b8f428850c147d78997d08a4d3b72c43e848baa8
Onifade Monsuru <onifade617@gmail.com>
2025-02-28 15:13:09 +0100

    Update settings.py

--- trees_project/settings.py
+++ trees_project/settings.py
@@ -214,3 +214,7 @@ if ENVIRONMENT == 'production':
     SESSION_COOKIE_SECURE = True
     CSRF_COOKIE_SECURE = True
     
+import dj_database_url
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
